b071870661624032fd21bb53b3b6bc27d89b832b
Oleksandr Orlov <alexanderorlovkh@gmail.com>
2024-03-18 20:58:08 +0200

    Created CrudService.java wrote comments for it and it`s methods. Closes #6 .

---
+++ src/main/java/orlov641p/khai/edu/com/enugaenglishlearningappbackend/services/CrudService.java
@@ -0,0 +1,50 @@
+package orlov641p.khai.edu.com.enugaenglishlearningappbackend.services;
+
+import java.util.Set;
+
+/**
+ * The CrudService interface provides CRUD (Create, Read, Update, Delete) operations
+ * for managing entities of type T.
+ *
+ * @param <T>  The type of entities managed by the service.
+ * @param <ID> The type of the entity's identifier.
+ */
+public interface CrudService<T, ID> {
+
+    /**
+     * Retrieves all entities of type T.
+     *
+     * @return A set containing all entities.
+     */
+    Set<T> findAll();
+
+    /**
+     * Retrieves an entity by its identifier.
+     *
+     * @param id The identifier of the entity to retrieve.
+     * @return The entity with the specified identifier, or null if not found.
+     */
+    T findById(ID id);
+
+    /**
+     * Saves a new entity or updates an existing one.
+     *
+     * @param object The entity to save or update.
+     * @return The saved or updated entity.
+     */
+    T save(T object);
+
+    /**
+     * Deletes the specified entity.
+     *
+     * @param object The entity to delete.
+     */
+    void delete(T object);
+
+    /**
+     * Deletes an entity by its identifier.
+     *
+     * @param id The identifier of the entity to delete.
+     */
+    void deleteById(ID id);
+}
