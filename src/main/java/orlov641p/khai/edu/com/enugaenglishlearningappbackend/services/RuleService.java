607118c5e33b7b7af4254e706dfa8629db323385
Oleksandr Orlov <alexanderorlovkh@gmail.com>
2024-03-18 21:02:29 +0200

    Created RuleService.java. Closes #7 .

---
+++ src/main/java/orlov641p/khai/edu/com/enugaenglishlearningappbackend/services/RuleService.java
@@ -0,0 +1,12 @@
+package orlov641p.khai.edu.com.enugaenglishlearningappbackend.services;
+
+import orlov641p.khai.edu.com.enugaenglishlearningappbackend.models.Rule;
+
+/**
+ * The RuleService interface extends CrudService to provide CRUD (Create, Read, Update, Delete)
+ * operations specific to Rule entities.
+ */
+
+public interface RuleService extends CrudService<Rule, Long>{
+
+}
