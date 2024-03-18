5179a14430a8d4bc2d504d19047fb0bd5ae69f88
Oleksandr Orlov <alexanderorlovkh@gmail.com>
2024-03-18 14:56:23 +0200

    Initialized Spring Application Closes #1

---
+++ src/main/java/orlov641p/khai/edu/com/enugaenglishlearningappbackend/EnugaEnglishLearningAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package orlov641p.khai.edu.com.enugaenglishlearningappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnugaEnglishLearningAppBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnugaEnglishLearningAppBackEndApplication.class, args);
+    }
+
+}
