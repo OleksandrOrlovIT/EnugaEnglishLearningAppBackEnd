f9645b89b3d01ff0507e9c36edca0f40b42cf86c
Oleksandr Orlov <alexanderorlovkh@gmail.com>
2024-03-24 19:50:54 +0200

    Created a repository, service and service implementation for Question Entity . Closes #14

---
+++ src/main/java/orlov641p/khai/edu/com/enugaenglishlearningappbackend/services/QuestionService.java
@@ -0,0 +1,7 @@
+package orlov641p.khai.edu.com.enugaenglishlearningappbackend.services;
+
+import orlov641p.khai.edu.com.enugaenglishlearningappbackend.models.Question;
+
+public interface QuestionService extends CrudService<Question, Long>{
+
+}
---
+++ src/main/java/orlov641p/khai/edu/com/enugaenglishlearningappbackend/services/impl/QuestionServiceImpl.java
@@ -0,0 +1,44 @@
+package orlov641p.khai.edu.com.enugaenglishlearningappbackend.services.impl;
+
+import orlov641p.khai.edu.com.enugaenglishlearningappbackend.models.Question;
+import orlov641p.khai.edu.com.enugaenglishlearningappbackend.repositories.QuestionRepository;
+import orlov641p.khai.edu.com.enugaenglishlearningappbackend.services.QuestionService;
+
+import java.util.List;
+
+public class QuestionServiceImpl implements QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public QuestionServiceImpl(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    @Override
+    public List<Question> findAll() {
+        return questionRepository.findAll();
+    }
+
+    @Override
+    public Question findById(Long id) {
+        return questionRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Question save(Question question) {
+        if (question == null) {
+            return null;
+        }
+        return questionRepository.save(question);
+    }
+
+    @Override
+    public void delete(Question question) {
+        questionRepository.delete(question);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        questionRepository.deleteById(id);
+    }
+}
