0020c5c68f9fd3044230f33261afb46ef82d25c3
Oleksandr Orlov <oleksandrorlovit@gmail.com>
2024-06-15 17:27:58 +0300

    Added exception handling for NoResourceFoundException and MethodArgumentTypeMismatchException Closes #62

--- src/main/java/orlov641p/khai/edu/com/enugaenglishlearningappbackend/exception/GlobalExceptionHandler.java
+++ src/main/java/orlov641p/khai/edu/com/enugaenglishlearningappbackend/exception/GlobalExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.nio.file.AccessDeniedException;
 import java.rmi.AccessException;
@@ -24,6 +26,20 @@ import java.util.Map;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseBody
+    public ResponseEntity<?> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        logException("MethodArgumentTypeMismatchException occurred", ex);
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "BAD_REQUEST", ex.getMessage());
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    @ResponseBody
+    public ResponseEntity<?> handleNoResourceFoundException(NoResourceFoundException ex) {
+        logException("NoResourceFoundException occurred", ex);
+        return buildErrorResponse(HttpStatus.NOT_FOUND, "NOT_FOUND", ex.getMessage());
+    }
 
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseBody
